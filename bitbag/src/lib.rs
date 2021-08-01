2d9febd05cae889a6890e228ca18560cc51f65b9
Aatif Syed <aatif@aatifsyed.uk>
2021-08-01 02:53:23 +0100

    how about a derive macro now, and fix builder

--- bitbag/src/lib.rs
+++ bitbag/src/lib.rs
@@ -1,11 +1,12 @@
 //! This crate provides [`BitBag`], a type intended for abstracting over [field-less enums](https://doc.rust-lang.org/rust-by-example/custom_types/enum/c_like.html)
 
-use derive_more::Deref;
+pub use bitbag_derive::BitBaggable;
+use derive_more::{Binary, Deref};
 use num::{PrimInt, Zero};
 use std::{
     any::type_name,
     fmt::{Binary, Debug, Display},
-    ops::{BitAnd, BitAndAssign, BitOr},
+    ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign},
 };
 use strum::IntoEnumIterator;
 
@@ -15,22 +16,31 @@ use std::ops::Deref;
 /// For an enum to be put inside a [`BitBag`]
 /// - We must tell the type system the primitive in `#[repr(primitive)]`
 /// - We must be able to convert (from any variant) into that primitive
+///
+/// You can derive this with the [`BitBaggable`] derive macro
 pub trait BitBaggable: Into<Self::Repr> {
     type Repr: PrimInt;
 }
 
 /// Wraps a primitive, with helper methods for checking flags.
 /// [`Deref`]s to the primitive if you wish to access it
-#[derive(Clone, Copy, Debug, Default, Deref)]
+#[derive(Clone, Copy, Debug, Deref, Binary)]
 #[repr(transparent)]
 pub struct BitBag<Flags: BitBaggable> {
     inner: Flags::Repr,
 }
 
+impl<Flags: BitBaggable> Default for BitBag<Flags> {
+    fn default() -> Self {
+        Self {
+            inner: Zero::zero(),
+        }
+    }
+}
+
 impl<Flag: BitBaggable> BitBag<Flag>
 where
-    Flag::Repr: BitAndAssign<Flag::Repr>,
-    BitBag<Flag>: Copy,
+    Flag::Repr: BitAndAssign<Flag::Repr> + BitOrAssign<Flag::Repr>,
 {
     /// Return `true` if the flag is set
     pub fn is_set(&self, flag: Flag) -> bool {
@@ -38,21 +48,21 @@ where
         self.inner.bitand(flag) == flag
     }
     /// Set the flag
-    pub fn set(&mut self, flag: Flag) -> Self {
+    pub fn set(&mut self, flag: Flag) -> &mut Self {
         let flag = flag.into();
-        self.inner.bitand_assign(flag);
-        *self
+        self.inner.bitor_assign(flag);
+        self
     }
     /// Unset the flag
-    pub fn unset(&mut self, flag: Flag) -> Self {
+    pub fn unset(&mut self, flag: Flag) -> &mut Self {
         let flag = flag.into();
         self.inner.bitand_assign(!flag);
-        *self
+        self
     }
     /// Unset all flags
-    pub fn clear_all(&mut self) -> Self {
+    pub fn clear_all(&mut self) -> &mut Self {
         self.inner.set_zero();
-        *self
+        self
     }
     /// Don't check the primitive for non-flag bits
     pub fn new_unchecked(prim: Flag::Repr) -> Self {
@@ -65,8 +75,6 @@ where
     Flag: IntoEnumIterator,
 {
     /// Check the bits of `prim`, and return an [`NonFlagBits`] error if it has bits set which can't be represented by a flag.
-    ///
-    /// Note that the [IntoIterator<Item = Flag>] must cycle through the enum variants for this to function correctly
     pub fn new(prim: Flag::Repr) -> Result<Self, NonFlagBits<Flag::Repr>> {
         let mask = Flag::iter()
             .map(Into::into)
@@ -83,7 +91,7 @@ where
 impl<Flag: BitBaggable> IntoIterator for BitBag<Flag>
 where
     Flag: IntoEnumIterator + Copy,
-    Flag::Repr: BitAndAssign,
+    Flag::Repr: BitAndAssign + BitOrAssign,
 {
     type Item = Flag;
 
@@ -99,7 +107,7 @@ where
 impl<Flag: BitBaggable> IntoIterator for &BitBag<Flag>
 where
     Flag: IntoEnumIterator + Copy,
-    Flag::Repr: BitAndAssign,
+    Flag::Repr: BitAndAssign + BitOrAssign,
     BitBag<Flag>: Copy,
 {
     type Item = Flag;
@@ -153,7 +161,7 @@ impl<Flag: BitBaggable> Iterator for BitBagIterator<Flag>
 where
     Flag: IntoEnumIterator,
     Flag: Copy,
-    Flag::Repr: BitAndAssign,
+    Flag::Repr: BitAndAssign + BitOrAssign,
 {
     type Item = Flag;
 
@@ -172,26 +180,17 @@ mod tests {
     use std::collections::HashSet;
 
     use super::*;
+    use crate as bitbag;
     use anyhow::Result;
     use strum::EnumIter;
 
-    #[derive(Debug, Copy, Clone, EnumIter, PartialEq, Eq, Hash)]
+    #[derive(Debug, Copy, Clone, EnumIter, PartialEq, Eq, Hash, BitBaggable)]
     #[repr(u8)]
     enum FooFlags {
         A = 0b0000_0001,
         B = 0b0000_0010,
         C = 0b0000_0100,
         D = 0b0000_1000,
-    }
-
-    impl BitBaggable for FooFlags {
-        type Repr = u8;
-    }
-
-    impl Into<u8> for FooFlags {
-        fn into(self) -> u8 {
-            self as _
-        }
     }
 
     #[test]
@@ -234,6 +233,22 @@ mod tests {
         let mut flags = bag.into_iter().collect::<Vec<_>>();
         assert!(flags.len() == 1);
         assert!(matches!(flags.pop(), Some(FooFlags::A)));
+        Ok(())
+    }
+
+    #[test]
+    fn manually_set() -> Result<()> {
+        let mut bag = BitBag::<FooFlags>::default();
+        bag.set(FooFlags::A).set(FooFlags::B);
+        assert_eq!(*bag, 0b0000_0011);
+        Ok(())
+    }
+
+    #[test]
+    fn manually_unset() -> Result<()> {
+        let mut bag = BitBag::<FooFlags>::new_unchecked(0b0000_0011);
+        bag.unset(FooFlags::A);
+        assert_eq!(*bag, 0b0000_0010);
         Ok(())
     }
 }
